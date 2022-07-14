77bdf068cad4b51fd353f0b7efd9118421e0dc61
Dominique Zeise <d.zei@gmx.de>
2022-07-14 23:06:11 +0200

    + Visulaization

--- baum_welch.py
+++ baum_welch.py
@@ -1,40 +1,20 @@
-import pandas as pd
+import itertools
 import numpy as np
-import matplotlib.pyplot as plt
-from pandas.plotting import table
-import dataframe_image as dfi
-
-
-"""SVG VORLAGE
-    <svg>
-    <symbol id="player">
-        <circle cx="225" cy="25" r="20" stroke="black" stroke-width="2" fill="red"/>
-    </symbol>
-    <path id="myarrow" d="M5,15 H25 V5 L45,25 L25,45 V35 H5 z"  fill="yellow" stroke="#ff6666" stroke-width="5" stroke-linejoin="round" />
-    <use href="#myarrow" x="10" fill="red" />
-
-    <circle id="myCircle" cx="50" cy="150" r="40" stroke="blue"/>
-    <use href="#myCircle" x="50" fill="blue"/>
-    <use href="#myCircle" x="100" fill="red" stroke="red"/>
-    <use href="#player" />
-    </svg>
-
-"""
-
+import pandas as pd
 
 
 class HiddenMarkovModel:
 
     def __init__(self):
-        self.states = [",", "A", "T", "G", "C"]  # V
-        self.transitions = [0,1,2] # S
+        self.SYMBOLS = [",", "A", "T", "G", "C"]  # V
+        self.STATES = [0,1,2] # S
 
-        self.a_matrix = np.zeros((len(self.transitions), len(self.transitions)))
-        self.b_matrix = np.zeros((len(self.transitions), len(self.states)))
+        self.a_matrix = np.zeros((len(self.STATES), len(self.STATES)))
+        self.b_matrix = np.zeros((len(self.STATES), len(self.SYMBOLS)))
         #! remove next line later
         self.sequence_output()
-        self.alpha = np.zeros((len(self.transitions), len(self.output)))
-        self.beta = np.zeros((len(self.transitions), len(self.output)))
+        self.alpha = np.zeros((len(self.STATES), len(self.OUTPUT)))
+        self.beta = np.zeros((len(self.STATES), len(self.OUTPUT)))
 
         self.fill_a_matrix()
         self.fill_b_matrix()
@@ -44,22 +24,22 @@ class HiddenMarkovModel:
         # Transition Probabilities
         self.a_matrix = np.array([
             [0, 0.5, 0.5],
-            [0.25, 0.5, 0.25],
-            [1/3, 0, 2/3]])
+            [0, 0.25, 0.75],
+            [1/2, 1/4, 1/4]])
 
     def fill_b_matrix(self):
         # Emission Probabilities
         self.b_matrix = np.array([
             [1, 0, 0, 0, 0],
-            [0, 1/8, 1/8, 1/4, 1/2],
-            [0, 1/2, 1/4, 1/8, 1/8]])
-        self.b_matrix = pd.DataFrame(self.b_matrix, columns=self.states)
+            [0, 1/3, 1/3, 1/6, 1/6],
+            [0, 1/6, 1/3, 1/6, 1/3]])
+        self.b_matrix = pd.DataFrame(self.b_matrix, columns=self.SYMBOLS)
 
     def sequence_output(self):
-        self.output = ",CGG,"
+        self.OUTPUT = ",CGG,"
 
     def forward(self):
-        self.alpha = np.zeros((len(self.transitions), len(self.output)))
+        self.alpha = np.zeros((len(self.STATES), len(self.OUTPUT)))
         self.alpha[0][0] = 1
         # start at second row (first is initial), start at index 1 because the first row was skipped
         for row_index, row in enumerate(self.alpha.T[1:], 1):
@@ -69,14 +49,14 @@ class HiddenMarkovModel:
                 a_matrix_row = np.copy(self.a_matrix.T[cell_index])
                 matrix_multiply_result_sum = np.dot(last_alpha_row, a_matrix_row)
 
-                current_nucleo = self.output[row_index]
+                current_nucleo = self.OUTPUT[row_index]
                 nucleo_probability = self.b_matrix[current_nucleo][cell_index]
                 # 4 ndigits
                 cell_result = round(matrix_multiply_result_sum * nucleo_probability, 8)
                 self.alpha.T[row_index][cell_index] = cell_result
 
     def backward(self):
-        self.beta = np.zeros((len(self.transitions), len(self.output)))
+        self.beta = np.zeros((len(self.STATES), len(self.OUTPUT)))
         self.beta[0][-1] = 1
         # start at second row (first is initial), start at index 2 because the last row was skipped and negative starts at -1,
         # [1::-1] means backward loop to second row
@@ -85,7 +65,7 @@ class HiddenMarkovModel:
 
                 a_matrix_col = np.copy(self.a_matrix[row_index])
 
-                next_nucleo = self.output[-col_index+1]
+                next_nucleo = self.OUTPUT[-col_index+1]
                 b_matrix_col = np.copy(self.b_matrix[next_nucleo].to_numpy())
                 next_beta_column = np.copy(self.beta.T[-col_index+1])
                 arrays = [a_matrix_col, next_beta_column, b_matrix_col]
@@ -95,73 +75,172 @@ class HiddenMarkovModel:
                 cell_result = round(matrix_multiply_result_sum, 8)
                 self.beta.T[-col_index][row_index] = cell_result
 
-    def create_image(self, df: pd.DataFrame):
-        df = pd.DataFrame(df, columns=[x for x in self.output])
+    def create_image(self, matrix_name: str, loop: int):
+        WIDTH = 1280
+        HEIGHT = 800
 
-        fig, ax = plt.subplots(figsize=(12, 2))  # set size frame
-        ax.xaxis.set_visible(False)  # hide the x axis
-        ax.yaxis.set_visible(False)  # hide the y axis
-        ax.set_frame_on(False)  # no visible frame, uncomment if size is ok
-        tabla = table(ax, df, loc='upper right', colWidths=[
-                    0.17]*len(df.columns))  # where df is your data frame
-        tabla.auto_set_font_size(False)  # Activate set fontsize manually
-        tabla.set_fontsize(12)  # if ++fontsize is necessary ++colWidths
-        tabla.scale(1.2, 1.2)  # change size table
-        plt.savefig('table.png', transparent=True)
+        HEIGHT_PARTS = len(self.STATES)
+        WIDTH_PARTS = len(self.OUTPUT)
+
+        PART_HEIGHT = HEIGHT / HEIGHT_PARTS
+        PART_WIDTH = WIDTH / WIDTH_PARTS
+
+        SMALER = min(PART_HEIGHT, PART_WIDTH)
+        CIRCLE_RADIUS = SMALER/4
+
+        MARKER_WIDTH = 10
+        MARKER_HIGHT = 7
+
+        x_coords = tuple(round(x, 1)
+                        for x in np.linspace(start=PART_WIDTH/2, stop=WIDTH-PART_WIDTH/2, num=WIDTH_PARTS))
+
+        y_coords = tuple(round(y, 1)
+                        for y in np.linspace(start=PART_HEIGHT/2, stop=HEIGHT-PART_HEIGHT/2, num=HEIGHT_PARTS))
+
+        coords = list(itertools.product(x_coords, y_coords))
+
+        circles = list(
+            f'  <circle cx = "{x}" cy = "{y}" r = "{CIRCLE_RADIUS}" stroke = "black" stroke-width = "3" fill="none"/>' for (x, y) in coords)
+
+        output_text = tuple(
+            f'  <text x="{x}" y="20" font-size="2em" text-anchor="middle" alignment-baseline="central">{symbol}</text>' for symbol, x in zip(self.OUTPUT, x_coords))
+
+
+        if matrix_name == "forward":
+            matrix = self.alpha
+            arrow = """<defs><marker id="arrow" markerWidth="10" markerHeight="7" refX="0" refY="3.5" orient="auto" markerUnits="strokeWidth"> <polygon points="0 0, 10 3.5, 0 7" /> </marker></defs>"""
+            line_settings = 'stroke="#000" stroke-width="2" marker-end="url(#arrow)"'
+            lines = tuple(f' <line x1="{x+CIRCLE_RADIUS}" y1="{y}" x2="{x+PART_WIDTH-CIRCLE_RADIUS-MARKER_WIDTH*2}" y2="{y}" {line_settings} />' for (x, y) in coords[:-len(self.STATES)])
+            down_lines = tuple(f' <line x1="{x+CIRCLE_RADIUS}" y1="{y}" x2="{x+PART_WIDTH-CIRCLE_RADIUS-MARKER_HIGHT}" y2="{y+PART_HEIGHT-MARKER_WIDTH*2}" {line_settings}/>' for index,
+                               (x, y) in enumerate(coords[:-len(self.STATES)]) if (index+1) % len(self.STATES) != 0)
+            full_down_lines = tuple(f' <line x1="{x+CIRCLE_RADIUS}" y1="{y}" x2="{x+PART_WIDTH-CIRCLE_RADIUS-MARKER_HIGHT}" y2="{y+2*PART_HEIGHT-MARKER_WIDTH*2}" {line_settings}/>' for index,
+                                    (x, y) in enumerate(coords[:-len(self.STATES)]) if (index+3) % len(self.STATES) == 0)
+            up_lines = tuple(f' <line x1="{x+CIRCLE_RADIUS}" y1="{y}" x2="{x+PART_WIDTH-CIRCLE_RADIUS-MARKER_HIGHT}" y2="{y-PART_HEIGHT+MARKER_WIDTH*2}" {line_settings}/>' for index,
+                             (x, y) in enumerate(coords[:-len(self.STATES)]) if (index) % len(self.STATES) != 0)
+            full_up_lines = tuple(f' <line x1="{x+CIRCLE_RADIUS}" y1="{y}" x2="{x+PART_WIDTH-CIRCLE_RADIUS-MARKER_HIGHT}" y2="{y-2*PART_HEIGHT+MARKER_WIDTH*2}" {line_settings}/>' for index,
+                                  (x, y) in enumerate(coords[:-len(self.STATES)]) if (index) % len(self.STATES) == 2)
+            circles[-len(self.STATES)] = circles[-len(self.STATES)].replace('fill="none"', 'fill="lightskyblue"')
+        elif matrix_name == "backward":
+            matrix = self.beta
+            arrow = """<defs><marker id="arrow" markerWidth="10" markerHeight="7" refX="10" refY="3.5" orient="auto"> <polygon points="10 0, 10 7, 0 3.5" /> </marker></defs>"""
+            line_settings = 'stroke="#000" stroke-width="2" marker-start="url(#arrow)"'
+            lines = tuple(
+                f' <line x1="{x+CIRCLE_RADIUS+MARKER_WIDTH*2}" y1="{y}" x2="{x+PART_WIDTH-CIRCLE_RADIUS}" y2="{y}" {line_settings} />' for (x, y) in coords[:-len(self.STATES)])
+            down_lines = tuple(f' <line x1="{x+CIRCLE_RADIUS+MARKER_HIGHT}" y1="{y+MARKER_WIDTH*2}" x2="{x+PART_WIDTH-CIRCLE_RADIUS}" y2="{y+PART_HEIGHT}" {line_settings}/>' for index,
+                               (x, y) in enumerate(coords[:-len(self.STATES)]) if (index+1) % len(self.STATES) != 0)
+            full_down_lines = tuple(f' <line x1="{x+CIRCLE_RADIUS+MARKER_HIGHT}" y1="{y+MARKER_WIDTH*2}" x2="{x+PART_WIDTH-CIRCLE_RADIUS}" y2="{y+2*PART_HEIGHT}" {line_settings}/>' for index,
+                                    (x, y) in enumerate(coords[:-len(self.STATES)]) if (index+3) % len(self.STATES) == 0)
+            up_lines = tuple(f' <line x1="{x+CIRCLE_RADIUS+MARKER_HIGHT}" y1="{y-MARKER_WIDTH*2}" x2="{x+PART_WIDTH-CIRCLE_RADIUS}" y2="{y-PART_HEIGHT}" {line_settings}/>' for index,
+                             (x, y) in enumerate(coords[:-len(self.STATES)]) if (index) % len(self.STATES) != 0)
+            full_up_lines = tuple(f' <line x1="{x+CIRCLE_RADIUS+MARKER_HIGHT}" y1="{y-MARKER_WIDTH*2}" x2="{x+PART_WIDTH-CIRCLE_RADIUS}" y2="{y-2*PART_HEIGHT}" {line_settings}/>' for index,
+                                  (x, y) in enumerate(coords[:-len(self.STATES)]) if (index) % len(self.STATES) == 2)
+            circles[0] = circles[0].replace('fill="none"', 'fill="lightskyblue"')
+
+        df_values = tuple(matrix.T.flatten())
+
+        texts = tuple(f"""<text text-anchor="middle" alignment-baseline="central">
+        <tspan x = "{x}" dy = "{y}">{round(text,5)}</tspan>
+        </text>
+        """ for text, (x, y) in zip(df_values, coords))
+
+        circle_colors = [(int(255-round(255*x, 0)), int(round(255*x, 0)), 0) for x in df_values]
+
+        for index, (color, circle) in enumerate(zip(circle_colors, circles)):
+            circles[index] = circle.replace('fill="none"', f'fill="rgb{color}"')
+
+        lines = '\n'.join(lines)
+        down_lines = '\n'.join(down_lines)
+        full_down_lines = '\n'.join(full_down_lines)
+        up_lines = '\n'.join(up_lines)
+        full_up_lines = '\n'.join(full_up_lines)
+        output_text = '\n'.join(output_text)
+        circles = '\n'.join(circles)
+        texts = '\n'.join(texts)
+
+
+        svg = f"""<svg height="{HEIGHT}" width="{WIDTH}">
+        {arrow}
+        {output_text}
+        {circles}
+        {lines}
+
+        {down_lines}
+
+        {full_down_lines}
+
+        {up_lines}
+
+        {full_up_lines}
+
+        {texts}
+        </svg>
+        """
+        with open(f"{matrix_name}_{loop}.svg", "w") as text_file:
+            text_file.write(svg)
+
+    def recalculate_a(self):
+        for row_index, row in enumerate(self.a_matrix):
+            for column_index, value in enumerate(row):
+                nucleos = self.OUTPUT[1:]
+                empty_b_list = []
+                for n in nucleos:
+                    empty_b_list.append(self.b_matrix[n][column_index])
+                b_matrix_values = np.array(empty_b_list)
+                arrays = [b_matrix_values, np.copy(
+                    self.alpha[row_index][:-1]), np.copy(self.beta[column_index][1:])]
+
+                counter = sum(np.prod(np.vstack(arrays), axis=0)
+                                * self.a_matrix[row_index][column_index])
+                denominator = np.dot(
+                    np.copy(self.alpha[row_index][1:]), np.copy(self.beta[row_index][1:]))
+
+                if (counter != 0) and (denominator != 0):
+                    result = counter / denominator
+                else:
+                    result = 0
+
+                self.a_matrix[row_index][column_index] = round(
+                    result, 8)
+
+    def recalculate_b(self):
+        for row_index, row in self.b_matrix.iloc[1:].iterrows():
+            for column_index, value in enumerate(row):
+                # find all indexes from output, where current b matrix column name appears
+                all_nucleo_indexes = [i for i, ltr in enumerate(
+                    self.OUTPUT) if ltr == self.b_matrix.columns[column_index]]
+
+                counter = np.dot(
+                    self.alpha[row_index][all_nucleo_indexes], self.beta[row_index][all_nucleo_indexes])
+                denominator = np.dot(
+                    self.alpha[row_index], self.beta[row_index])
+                if counter != 0 or denominator != 0:
+                    result = counter / denominator
+                else:
+                    result = 0
+                self.b_matrix[self.b_matrix.columns[column_index]
+                                ].iloc[row_index] = round(result, 8)
 
     def baum_welch(self):
         max_loops = 100
         loop = 1
         while loop < max_loops:
             last_alpha_result = self.alpha[0][-1]
+
             self.forward()
-            new_alpha_result = self.alpha[0][-1]
+            hmm.create_image("forward", loop)
+
+            new_alpha_result = round(self.alpha[0][-1], 3)
             if last_alpha_result == new_alpha_result:
                 print(f"Calculation stopped - stable Possibility : {round(new_alpha_result,8)*100}% after {loop} loops!\n")
                 return
+
             self.backward()
+            hmm.create_image("backward", loop)
 
-            def recalculate_a():
-                for row_index, row in enumerate(self.a_matrix):
-                    for column_index, value in enumerate(row):
-                        nucleos = self.output[1:]
-                        empty_b_list = []
-                        for n in nucleos:
-                            empty_b_list.append(self.b_matrix[n][column_index])
-                        b_matrix_values = np.array(empty_b_list)
-                        arrays = [b_matrix_values, np.copy(self.alpha[row_index][:-1]), np.copy(self.beta[column_index][1:])]
+            self.recalculate_a()
+            self.recalculate_b()
 
-                        counter = sum(np.prod(np.vstack(arrays), axis=0) * self.a_matrix[row_index][column_index])
-                        denominator = np.dot(
-                            np.copy(self.alpha[row_index][1:]), np.copy(self.beta[row_index][1:]))
-
-                        if (counter != 0) and (denominator != 0):
-                            result = counter / denominator
-                        else:
-                            result = 0
-
-                        self.a_matrix[row_index][column_index] = round(result, 8)
-
-            def recalculate_b():
-                for row_index, row in self.b_matrix.iloc[1:].iterrows():
-                    for column_index, value in enumerate(row):
-                        # find all indexes from output, where current b matrix column name appears
-                        all_nucleo_indexes = [i for i, ltr in enumerate(
-                            self.output) if ltr == self.b_matrix.columns[column_index]]
-
-                        counter = np.dot(
-                            self.alpha[row_index][all_nucleo_indexes], self.beta[row_index][all_nucleo_indexes])
-                        denominator = np.dot(self.alpha[row_index], self.beta[row_index])
-                        if counter != 0 or denominator != 0:
-                            result = counter / denominator
-                        else:
-                            result = 0
-                        self.b_matrix[self.b_matrix.columns[column_index]].iloc[row_index] = round(result,8)
-
-        recalculate_a()
-        recalculate_b()
-
-        loop += 1
+            loop += 1
 
     def check(self) -> bool:
         # Check if forward and backword worked
@@ -169,10 +248,10 @@ class HiddenMarkovModel:
 
     def __repr__(self) -> str:
         a_df = pd.DataFrame(
-            self.a_matrix, columns=self.transitions)
-        b_df = pd.DataFrame(self.b_matrix, columns=self.states)
-        alpha_df = pd.DataFrame(self.alpha, columns=[x for x in self.output])
-        beta_df = pd.DataFrame(self.beta, columns=[x for x in self.output])
+            self.a_matrix, columns=self.STATES)
+        b_df = pd.DataFrame(self.b_matrix, columns=self.SYMBOLS)
+        alpha_df = pd.DataFrame(self.alpha, columns=[x for x in self.OUTPUT])
+        beta_df = pd.DataFrame(self.beta, columns=[x for x in self.OUTPUT])
 
         rep = "Transition Probabilities - A-Matrix\n" + \
             f"{a_df}\n\n" + "Emission Probabilities - B-Matrix\n" + \
@@ -181,12 +260,11 @@ class HiddenMarkovModel:
         return rep
 
 
-hmm = HiddenMarkovModel()
-print(hmm)
 
-print("\n\n =========== BAUM-WELCH START ==============\n")
-hmm.baum_welch()
-print(hmm)
+if __name__ == '__main__':
+    hmm = HiddenMarkovModel()
+    print(hmm)
 
-
-# hmm.create_image(hmm.alpha)
+    print("\n\n =========== BAUM-WELCH START ==============\n")
+    hmm.baum_welch()
+    print(hmm)
